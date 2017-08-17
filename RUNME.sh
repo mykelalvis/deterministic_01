01ed01a5c97cd0d6a5ffa5e72759426eb10fd66f
@mykelalvis <mykel.alvis@gmail.com>
2017-08-17 15:24:01 -0500

    Add setup for gems

--- RUNME.sh
+++ RUNME.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-read -p "Enter your username : " username
-read -s -p "Enter your password : " password
+username=X
+password=Y
+# read -p "Enter your username : " username
+# read -s -p "Enter your password : " password
 
 
-GEMS_URL=https://gems.artifacts.io/repository/gems-releases-local
-gem sources --add ${GEMS_URL}
+GEMS_URL=https://${username}:${password}@artifacts.cotiviti.io/repository/gems-releases-local/
+GEMS_URL2=https://${username}:${password}@artifacts.cotiviti.io/repository/rubygems-public/
+gem sources --add ${GEMS_URL2}
 bundle config ${GEMS_URL} ${username}:${password}
+bundle config ${GEMS_URL2} ${username}:${password}
 
